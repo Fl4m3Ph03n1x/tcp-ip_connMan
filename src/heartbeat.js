ee0577c7188e5cf3c41436280ab854f2d0eb8a21
Fl4m3Ph03n1x <palmelapedro@gmail.com>
2017-06-14 07:53:54 +0000

    Adding docs

--- src/heartbeat.js
+++ src/heartbeat.js
@@ -2,6 +2,13 @@
 
 const isFunction = require("lodash.isfunction");
 
+/**
+ *  @public
+ *  @author Pedro Miguel P. S. Martins
+ *  @version 1.0.0
+ *  @module heartBeat
+ *  @desc   Runs a given ping function periodically and watches for timeouts.
+ */
 const heartBeatFactory = (aPing, aPong) => {
 
     const DEFAULT = {
@@ -15,11 +22,46 @@ const heartBeatFactory = (aPing, aPong) => {
         pong = aPong,
         timer,
         lastHeartbeatTime;
-
+    
+    /**
+     *  @public 
+     *  @func       hasTimedOut
+     *  @returns    {Boolean}   <code>true</code> if the heartbeat has timedout, 
+     *                          <code>false</code> otherwise. 
+     * 
+     *  @description    Used to detected if a heartbeat has timedout. A 
+     *                  heartbeat times out when it sends a ping, and receives 
+     *                  no pong after a given period of time. The timeout period
+     *                  can be manipulated via <code>setBeatTimeout</code>.
+     * @see             <code>setBeatTimeout</code>
+     */
     const hasTimedOut = () =>
         Date.now() - lastHeartbeatTime > timeout;
-
+    
+    /**
+     *  @public 
+     *  @func       getBeatInterval
+     *  @returns    {Number}        The current heartbeat interval.
+     * 
+     *  @description    Returns the current hearbeat interval. The heartbeat 
+     *                  interval is the interval at which the heartbeat will run
+     *                  the <code>ping</code> function.
+     */
     const getBeatInterval = () => interval;
+    
+    /**
+     *  @public 
+     *  @func   setBeatInterval
+     *  @param  {Number}        newInterval The new heartbeat interval.  
+     * 
+     *  @description    Sets the current heartbeat interval to the given one. 
+     *                  Note that setting the heartbeat interval will <b>not</b>
+     *                  affetct current heartbeat running. You must 
+     *                  <code>stop</code> them and then <code>start</code> them
+     *                  for the new interval to be applied.
+     * @see             <code>stop</code>
+     * @see             <code>start</code>
+     */
     const setBeatInterval = newInterval => {
         interval = newInterval;
     };

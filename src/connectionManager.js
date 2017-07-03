8a4fbf046105ad4ce7b77f22d82a4fe3b0dd9edc
Pedro Martins <palmelapedro@gmail.com>
2017-07-03 12:53:09 +0200

    Refactorized to code.

--- src/connectionManager.js
+++ src/connectionManager.js
@@ -1,9 +1,9 @@
-const net = require("net");
-const heartBeatFactory = require("heartbeatjs");
-const isFunction = require("lodash.isfunction");
-const errors = require("./errors.js");
+const net = require( "net" );
+const heartBeatFactory = require( "heartbeatjs" );
+const isFunction = require( "lodash.isfunction" );
+const errors = require( "./errors.js" );
 const cbException = errors.callbackNotAFunction;
-const connectHandlerNotAFunction = errors.connectHandlerNotAFunction;
+const connHandlerException = errors.connectHandlerNotAFunction;
 
 
 /**
@@ -14,13 +14,13 @@ const connectHandlerNotAFunction = errors.connectHandlerNotAFunction;
  *  @desc   Takes care of remote tcp-ip connections by attempting automatic
  *          reconnects and handling timeouts.
  */
-const connManager = (heartBeat = heartBeatFactory()) => {
+const connManager = ( heartBeat = heartBeatFactory() ) => {
 
-    if (heartBeat.getPing() === undefined)
-        heartBeat.setPing(Buffer.from([0x01]));
+    if ( heartBeat.getPing() === undefined )
+        heartBeat.setPing( Buffer.from( [ 0x01 ] ) );
 
-    if (heartBeat.getPong() === undefined)
-        heartBeat.setPong(Buffer.from([0x02]));
+    if ( heartBeat.getPong() === undefined )
+        heartBeat.setPong( Buffer.from( [ 0x02 ] ) );
 
     const eventFns = {
         onClose: () => {},
@@ -31,11 +31,11 @@ const connManager = (heartBeat = heartBeatFactory()) => {
 
     const connection = {
         socket: undefined,
-        connectFn: function(theOpts) {
-            return new Promise((resolve, reject) => {
-                const socket = net.createConnection(theOpts, () => resolve(socket));
-                socket.once("error", err => reject(err));
-            });
+        connectFn: function ( theOpts ) {
+            return new Promise( ( resolve, reject ) => {
+                const socket = net.createConnection( theOpts, () => resolve( socket ) );
+                socket.once( "error", err => reject( err ) );
+            } );
         },
         options: undefined,
         retriesCnt: 0
@@ -81,34 +81,32 @@ const connManager = (heartBeat = heartBeatFactory()) => {
      *  @see            <code>onRetry</code>
      *  @see            <code>onClose</code>
      */
-    const connect = async function(connectOpts) {
-        if (connectOpts === undefined)
-            throw new Error("connect must have options");
+    const connect = async function ( connectOpts ) {
+        if ( connectOpts === undefined )
+            throw new Error( "connect must have options" );
 
         connection.options = connectOpts;
         let done = false;
-        while (!done) {
+        while ( !done ) {
             try {
-                connection.socket = await connection.connectFn(connectOpts);
+                connection.socket = await connection.connectFn( connectOpts );
                 connection.retriesCnt = 0;
-                connection.socket.on("data", read);
-                eventFns.onOpen(true);
+                connection.socket.on( "data", read );
+                eventFns.onOpen( true );
                 done = true;
-            }
-            catch (err) {
+            } catch ( err ) {
                 connection.retriesCnt++;
-                eventFns.onRetry(err, connection.retriesCnt);
+                eventFns.onRetry( err, connection.retriesCnt );
             }
         }
-        heartBeat.onTimeout(reconnect);
-        heartBeat.start(pingFn);
+        heartBeat.onTimeout( reconnect );
+        heartBeat.start( pingFn );
     };
 
-    const pingFn = function() {
+    const pingFn = function () {
         try {
-            send(heartBeat.getPing());
-        }
-        catch (err) {
+            send( heartBeat.getPing() );
+        } catch ( err ) {
             //the connection died as we were pinging
             reconnect();
         }
@@ -117,7 +115,7 @@ const connManager = (heartBeat = heartBeatFactory()) => {
 
     const reconnect = () => {
         disconnect();
-        connect(connection.options); //attempt reconnect and restart cycle
+        connect( connection.options ); //attempt reconnect and restart cycle
     };
 
     /**
@@ -128,10 +126,10 @@ const connManager = (heartBeat = heartBeatFactory()) => {
      *                  and fires <code>onClose</code>
      *  @see            <code>onClose</code>
      */
-    const disconnect = function() {
+    const disconnect = function () {
         heartBeat.stop();
         connection.socket.destroy();
-        eventFns.onClose(false);
+        eventFns.onClose( false );
     };
 
     /**
@@ -144,16 +142,16 @@ const connManager = (heartBeat = heartBeatFactory()) => {
      *                  <code>false</code> otherwise. If a reconnection is taking
      *                  place, it will still return <code>false</code>.
      */
-    const isConnected = function() {
+    const isConnected = function () {
         return heartBeat.isBeating() && !connection.socket.destroyed;
     };
 
-    const read = function(data) {
-        if (data.equals(Buffer.from(heartBeat.getPong()))) {
+    const read = function ( data ) {
+        if ( data.equals( Buffer.from( heartBeat.getPong() ) ) ) {
             heartBeat.receivedPong();
             return;
         }
-        eventFns.onRead(data);
+        eventFns.onRead( data );
     };
 
     /**
@@ -166,11 +164,11 @@ const connManager = (heartBeat = heartBeatFactory()) => {
      *  @description    Sends a message to the target.
      *  @see            <code>isConnected</code>
      */
-    const send = function(message) {
-        if (connection.socket.destroyed)
-            throw new Error("Connection is down, message not delivered.");
+    const send = function ( message ) {
+        if ( connection.socket.destroyed )
+            throw new Error( "Connection is down, message not delivered." );
 
-        connection.socket.write(message);
+        connection.socket.write( message );
     };
 
     /**
@@ -184,10 +182,8 @@ const connManager = (heartBeat = heartBeatFactory()) => {
      *                  event is triggered, passing it the argument
      *                  <code>false</code>.
      */
-    const onClose = function(newFn) {
-        if (!isFunction(newFn))
-            throw cbException(newFn, "onClose");
-        eventFns.onClose = newFn;
+    const onClose = function ( newFn ) {
+        registerCallback( newFn, "onClose" );
     };
 
     /**
@@ -201,10 +197,8 @@ const connManager = (heartBeat = heartBeatFactory()) => {
      *                  event is triggered, passing it the argument
      *                  code>true</code>.
      */
-    const onOpen = function(newFn) {
-        if (!isFunction(newFn))
-            throw cbException(newFn, "onOpen");
-        eventFns.onOpen = newFn;
+    const onOpen = function ( newFn ) {
+        registerCallback( newFn, "onOpen" );
     };
 
     /**
@@ -218,10 +212,8 @@ const connManager = (heartBeat = heartBeatFactory()) => {
      *                  event is triggered, passing it the received data as
      *                  an argument.
      */
-    const onRead = function(newFn) {
-        if (!isFunction(newFn))
-            throw cbException(newFn, "onRead");
-        eventFns.onRead = newFn;
+    const onRead = function ( newFn ) {
+        registerCallback( newFn, "onRead" );
     };
 
     /**
@@ -235,10 +227,15 @@ const connManager = (heartBeat = heartBeatFactory()) => {
      *                  event is triggered, passing it the error that caused the
      *                  retry, as well as a count of the number of retries.
      */
-    const onRetry = function(newFn) {
-        if (!isFunction(newFn))
-            throw cbException(newFn, "onRetry");
-        eventFns.onRetry = newFn;
+    const onRetry = function ( newFn ) {
+        registerCallback( newFn, "onRetry" );
+    };
+
+    const registerCallback = ( newFn, eventName ) => {
+        if ( !isFunction( newFn ) )
+            throw cbException( newFn, eventName );
+
+        eventFns[ eventName ] = newFn;
     };
 
     /**
@@ -255,15 +252,13 @@ const connManager = (heartBeat = heartBeatFactory()) => {
      *                  function passed must return the socket used for the
      *                  connection.
      */
-    const setConnectFn = function(newFn) {
-        if (!isFunction(newFn))
-            throw connectHandlerNotAFunction(newFn);
+    const setConnectFn = function ( newFn ) {
+        if ( !isFunction( newFn ) )
+            throw connHandlerException( newFn );
         connection.connectFn = newFn;
     };
 
-
-
-    return Object.freeze({
+    return Object.freeze( {
         connect,
         disconnect,
         isConnected,
@@ -273,7 +268,7 @@ const connManager = (heartBeat = heartBeatFactory()) => {
         onRead,
         onRetry,
         setConnectFn
-    });
+    } );
 };
 
 module.exports = connManager;
